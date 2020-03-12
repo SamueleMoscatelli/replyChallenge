acf5bf348812b68b2c785391f14788e6531b6f7a
andreapozzoli <pozzoliandrea97@gmail.com>
2020-03-12 17:02:34 +0100

    ok

---
+++ replyChallenge Project/src/replyChallenge/ProjectManager.java
@@ -0,0 +1,33 @@
+package replyChallenge;
+
+public class ProjectManager {
+
+	public ProjectManager() {
+		
+	}
+	
+	public ProjectManager(String company, int bonus) {
+		this.company=company;
+		this.bonus=bonus;
+	}
+	
+	private String company;
+	private int bonus;
+	
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public int getBonus() {
+		return bonus;
+	}
+
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+
+	
+}
