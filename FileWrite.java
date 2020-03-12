51e4445e3c67882c94a1c3063234ee21870e4e3c
unknown <pietrohideki@gmail.com>
2020-03-12 16:16:16 +0100

    file handlers

---
+++ FileWrite.java
@@ -0,0 +1,47 @@
+
+import java.io.File;
+import java.io.FileWriter;   
+import java.io.IOException;
+
+public class FileWrite {
+
+	File file=null;
+	FileWriter fw=null;
+	private FileWrite()
+	{
+		
+	}
+	
+	static 
+	{
+		instance=new FileWrite();
+	}
+	
+	static FileWrite instance;
+	
+	static FileWrite getInstance()
+	{
+		return instance;
+	}
+	
+	public void createFile(String fullPath) throws IOException
+	{
+		if(fw!=null)
+		{
+			this.close();
+		}
+		file = new File(fullPath);
+		fw=new FileWriter(file);
+	}
+	
+	public void append(String app) throws IOException
+	{
+		fw.write(app);
+	}
+	
+	public void close() throws IOException
+	{
+		fw.close();
+	}
+	
+}
